81693a590d101343bc33c124dbee72fd07604647
Spencer <Spencer.Taber@yahoo.com>
2021-08-28 16:07:44 +0100

    pizza class and service build complete

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -3,7 +3,7 @@ namespace contoso_pizza.Models
     public class Pizza
     {
      public int Id {get; set; }
-     public string Name {get;}
+     public string Name {get; set;}
      public bool IsGlutenFree {get; set;}
     }
 }
